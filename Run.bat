266a4711855d6d6f1a783527e1f76b3f6817a7a5
Raykar-Poonam <159470936+Raykar-Poonam@users.noreply.github.com>
2026-04-18 20:56:47 +0530

    Update Run.bat

--- Run.bat
+++ Run.bat
@@ -1,5 +1,5 @@
 pytest -v -s -p no:warnings --html=HtmlReports/Poonam_01111.html -=alluredir="AllureReports"
 
->pytest -v -s -p no:warnings --html=HtmlReports/Poonam_01111.html --alluredir="AllureReports" -n=4 -m ddtparams
+pytest -v -s -p no:warnings --html=HtmlReports/Poonam_01111.html --alluredir="AllureReports" -n=4 -m ddtparams
 
->pytest -v -s -p no:warnings --html=HtmlReports/Poonam_01111.html --alluredir="AllureReports" -m ddtexcel
+pytest -v -s -p no:warnings --html=HtmlReports/Poonam_01111.html --alluredir="AllureReports" -m ddtexcel
